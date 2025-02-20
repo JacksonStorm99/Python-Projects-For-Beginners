0b5fabf41c107844935825014aa55920b518a565
Jackson Storm <jacksonstorm2050@gmail.com>
2025-02-20 17:38:22 +0000

    Added Code

---
+++ RandomNumberProject.py
@@ -0,0 +1,7 @@
+input("Hey, Wanna Play A Game?")
+input("Alright, Let's Begin!")
+Number_Guess = input("Please Input Any Number From 1-15.")
+if Number_Guess == 6:
+    print("Yay, you got it right.")
+else:
+    print("Oh, uhhhhhhhh you didn't get it right, sorry.")
