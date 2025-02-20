cd38e3910d59f66e9125a012c6550728a7d60f43
Jackson Storm <jacksonstorm2050@gmail.com>
2025-02-20 17:44:51 +0000

    Added Comments That Add Understandable Info

--- RandomNumberProject.py
+++ RandomNumberProject.py
@@ -1,7 +1,7 @@
-input("Hey, Wanna Play A Game?")
+input("Hey, Wanna Play A Game?") # These are the Questions
 input("Alright, Let's Begin!")
-Number_Guess = input("Please Input Any Number From 1-15.")
-if Number_Guess == 6:
+Number_Guess = input("Please Input Any Number From 1-15.") # Stored In A Variable
+if Number_Guess == 6:   # If Statement Introducing The Fact: If the player answers with this the print __________ , if not print __________
     print("Yay, you got it right.")
 else:
     print("Oh, uhhhhhhhh you didn't get it right, sorry.")
