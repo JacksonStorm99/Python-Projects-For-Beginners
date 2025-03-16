c3c17cdfa391a52f90974b7898a29129ab0c248c
JacksonStorm99 <jacksonstorm2050@gmail.com>
2025-03-16 08:49:36 +0000

    Made RandomNumberProject more better by adding random library

--- RandomNumberProject.py
+++ RandomNumberProject.py
@@ -1,7 +1,19 @@
 import random
 
 
-input("Hey, Wanna Play A Game?") # These are the Questions
-input("Alright, Let's Begin!")
-Number_Guess = input("Please Input Any Number From 1-20.") # Stored In A Variable
+Question1 = input("Hey, Wanna Play A Game? (Y/N) ").lower() # These are the Questions
+if Question1 == "y":
+    print("Ok, let's begin! ")
+
+if Question1 == "n":
+    quit()
+
+Number_Guess = input("Please Input Any Number From 1-20. ") # Stored In A Variable
 Number_Guess = int(Number_Guess) # Converted To An Integer
+RandomNumber = random.randint(1,20) 
+if Number_Guess == RandomNumber:
+    print("Yay, you got it right!")
+
+else: 
+    print("Oh, you got it wrong :(")
+    print("The number was" , RandomNumber)
